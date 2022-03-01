b6945e38d39c73fb99bfc6fe188faa67c5d7dcde
Artem Lobovskii <a.lobovskiy@gmail.com>
2022-03-01 14:32:36 +0300

    add css trails

--- src/components/pages/tree/tree-node.js
+++ src/components/pages/tree/tree-node.js
@@ -1,4 +1,4 @@
-function TreeNode({ data }) {
+function TreeNode({ data, level = 1 }) {
   if (!data?.length) {
     return null;
   }
@@ -6,11 +6,11 @@ function TreeNode({ data }) {
   return (
     data.map((item, i) => {
       const children = item?.children?.length ?
-        <TreeNode data={item.children} /> :
+        <TreeNode data={item.children} level={level + 1} /> :
         null;
 
       return (
-        <div key={ i }>
+        <div key={ i } data-level={level} >
           { item.firstName } { item.lastName }
           { children }
         </div>
