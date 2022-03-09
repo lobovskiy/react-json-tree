20d49774f1489bae586302d65712e74d15216cf7
Artem Lobovskii <a.lobovskiy@gmail.com>
2022-03-09 12:06:51 +0300

    get json mocks in sync mode

--- src/api/get-table.js
+++ src/api/get-table.js
@@ -1,10 +1,13 @@
-import getData from './get-data';
+// import getData from './get-data';
+import getDataSync from './get-data-sync';
 import { tableDTO } from '../dto';
 
 async function getTable() {
-	const data = await getData('http://localhost:3004/family');
+	// const data = await getData('http://localhost:3004/family');
+  const data = await getDataSync('MOCK_DATA.json');
 
-  const tableData = data.map(item => tableDTO(item));
+  // const tableData = data.map(item => tableDTO(item));
+  const tableData = data.family.map(item => tableDTO(item));
 
 	return await tableData;
 }
--- src/api/get-tree.js
+++ src/api/get-tree.js
@@ -1,11 +1,14 @@
-import getData from './get-data';
+// import getData from './get-data';
+import getDataSync from './get-data-sync';
 import { treeDTO } from '../dto';
 import { getRootParents, createTreeArray } from '../helpers';
 
 async function getTree() {
-	const data = await getData('http://localhost:3004/family');
+	// const data = await getData('http://localhost:3004/family');
+  const data = await getDataSync('MOCK_DATA.json');
 
-  const treeData = data.map(item => treeDTO(item));
+  // const treeData = data.map(item => treeDTO(item));
+  const treeData = data.family.map(item => treeDTO(item));
 
   const rootParents = getRootParents(treeData);
 
