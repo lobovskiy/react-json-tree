a7979ce13773d11df554341fd6ab2a7f96b19c14
Artem Lobovskii <a.lobovskiy@gmail.com>
2022-03-24 17:19:28 +0300

    refactor saga delays

--- src/saga/familySaga.js
+++ src/saga/familySaga.js
@@ -1,17 +1,17 @@
-import { put, takeEvery, call } from 'redux-saga/effects';
+import { put, takeEvery, call, delay } from 'redux-saga/effects';
 import { startLoading, addFamilyTable, FETCH_FAMILY_TABLE, addFamilyTree, FETCH_FAMILY_TREE } from '../store/reducer';
 import { getTable, getTree } from '../api';
 
 function* fetchFamilyTableWorker() {
   yield put(startLoading());
-  yield call(() => new Promise(res => setTimeout(res, 1000)));
+  yield delay(1000);
   const data = yield call(() => getTable());
   yield put(addFamilyTable(data));
 }
 
 function* fetchFamilyTreeWorker() {
   yield put(startLoading());
-  yield call(() => new Promise(res => setTimeout(res, 1000)));
+  yield delay(1000);
   const data = yield call(() => getTree());
   yield put(addFamilyTree(data));
 }
